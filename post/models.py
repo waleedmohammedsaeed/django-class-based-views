b23e1432ec4d0090138cf46e440fa641fc702239
waleed mohammed <waleed_mohammed2008@hotmail.com>
2020-06-21 16:19:52 +0300

    add post and imsges

--- post/models.py
+++ post/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 
 class post(models.Model):
     title =   models.CharField(max_length=50)
     content = models.TextField(max_length=1000)
     created_at = models.DateTimeField(default=timezone.now)
-    image = models.ImageField(upload_to='post')
+    image = models.ImageField(upload_to='images/')
     
 
     class Meta:
@@ -15,8 +16,8 @@ class post(models.Model):
         verbose_name_plural = ("posts")
 
     def __str__(self):
-        return self.name
+        return self.title
 
-    def get_absolute_url(self):
-        return reverse("post_detail", kwargs={"pk": self.pk})
+#    def get_absolute_url(self):
+#        return reverse("post_detail", kwargs={"pk": self.pk})
 
--- post/views.py
+++ post/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib import admin
 
 # Create your views here.
+
+
+
+def  post_list(requset):
+    #post_list = post.objects.all()
+    return render(requset , 'post/list.html', {'post_list' : post_list})
+
+def  post_detail(requset):
+    pass    
